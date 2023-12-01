44a79d9eba1c2a893bd1e044c29cdd338a4062ee
DahVoid <cederlund.oscar@gmail.com>
2023-12-01 18:39:15 +0100

    Code cleaining

--- app_back/app.py
+++ app_back/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, jsonify, render_template
 import pymongo
 from pymongo import MongoClient
-import time
 
 app = Flask(__name__)
 
@@ -28,33 +27,6 @@ def add(a, b):
 def get_str():
 
     return "Hello from backend"
-
-
-# @app.route("/hello", methods=("GET", "POST"))
-# def hello():
-#     """
-#     This function just responds to the browser ULR
-#     """
-#     #
-#     # if request.method == "POST":
-#     #     content = time.time()
-
-#     # return render_template("index.html")
-
-#     animal = {"Type": "Lizard", "Name": "Plato", "Time": str(time.time())}
-
-#     animal_id = animals.insert_one(animal).inserted_id
-#     return jsonify({"Message": str(db.list_collection_names())})
-
-
-# @app.route("/animals")
-# def get_animals():
-#     out_str = "count: " + str(animals.count_documents({})) + "<br>"
-#     for animal in animals.find():
-#         out_str += str(animal)
-#         out_str += "<br>"
-
-#     return out_str
 
 
 if __name__ == "__main__":
--- app_front/app.py
+++ app_front/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, jsonify, render_template, request, url_for, redirect
 import pymongo
 from pymongo import MongoClient
-import time
 import json
 import requests
 
@@ -26,9 +25,6 @@ def empty_db():
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
-    # Get string from backend
-    # res = ""
-    # res = requests.get("http://flask-app-back:5000/get_str")
     r = False
     if request.form:
         if request.method == "POST":
@@ -36,8 +32,6 @@ def add():
             b = request.form["b"]
             response = requests.get(
                 "http://flask-app-back:5000/add/" + a + "/" + b)
-            # response_dict = json.loads(response.text)
-            # r = response_dict["Result"]
             r = response.text
 
     # Get history from db
@@ -46,33 +40,6 @@ def add():
         str_row = row["A"] + " + " + row["B"] + " = " + row["R"]
         hist.append(str_row)
     return render_template("index.html", history=hist, result=r)
-
-
-# @app.route("/hello", methods=("GET", "POST"))
-# def hello():
-#     """
-#     This function just responds to the browser ULR
-#     """
-#     #
-#     # if request.method == "POST":
-#     #     content = time.time()
-
-#     # return render_template("index.html")
-
-#     animal = {"Type": "Lizard", "Name": "Plato", "Time": str(time.time())}
-
-#     animal_id = animals.insert_one(animal).inserted_id
-#     return jsonify({"Message": str(db.list_collection_names())})
-
-
-# @app.route("/animals")
-# def get_animals():
-#     out_str = "count: " + str(animals.count_documents({})) + "<br>"
-#     for animal in animals.find():
-#         out_str += str(animal)
-#         out_str += "<br>"
-
-#     return out_str
 
 
 if __name__ == "__main__":
